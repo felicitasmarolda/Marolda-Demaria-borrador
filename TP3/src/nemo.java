bed6fceddcf40f46a81184b09cf8b02cc9678bb9
Felicitas <fmarolda@udesa.edu.ar>
2023-10-04 13:48:35 -0300

    primera parte nemo

--- TP3/src/nemo.java
+++ TP3/src/nemo.java
@@ -1,5 +1,64 @@
 package nemo;
 
-public class nemo {
+public class Nemo {
+	
+	private int height;
+	private int distX;
+	private int distY;
+	private int direction;
+	
+	public Nemo (int height, int distX, int distY) {
+		this.height = height;
+		this.distX = distX;
+		this.distX = distX;
+		this.direction = direction;
+		
+	}
+	// ver si comando perpetuado es string[] o string 
+	public Nemo commandsForNemo(String[] commandChain) {
+		for (int i = 0; i < commandChain.length; i++) {
+            String command = commandChain[i];
+            if (command == "d") {
+            	int newHeight = height - 1;
+            }
+            if (command == "u") {
+            	int newHeight = height + 1;
+            }
+            if (command == "l") {
+                if (command == "r") {
+                	if (direction <= 180) {
+                		int newDirection = direction + 90;}
+                	else if(direction == 270) {
+                		int newDirection = 0;}
+                	}
+            	int newDirection = direction + 90;
+            }
+            if (command == "r") {
+            	if (direction >= 90) {
+            		int newDirection = direction - 90;}
+            	else if(direction == 0) {
+            		int newDirection = 270;}
+            	}
+              
+            if (command == "f") {
+            	if (direction == 0) {
+            		int newDistX = distX + 1;
+            	} 
+            	if (direction == 90) {
+            		int newDistY = distY + 1;
+            	}
+            	if (direction == 180) {
+            		int newDistX = distX - 1;
+            	}
+            	if (direction == 270) {
+            		int newDistY = distY - 1;
+            	} 
+        }
+            if (command == "m") {
+            	//entender que significa "librar la capsula"
+            	}
+            }
+		return this;
+	}
 
 }
