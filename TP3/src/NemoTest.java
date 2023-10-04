0dc66f3c1ac92daecf55c5e693585242311356af
Felicitas <fmarolda@udesa.edu.ar>
2023-10-04 19:00:29 -0300

    mas tests

--- TP3/src/NemoTest.java
+++ TP3/src/NemoTest.java
@@ -1,18 +1,32 @@
 package nemo;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
-import pharma.Pharmacy;
+import org.junit.jupiter.api.function.Executable;
 
 
 public class NemoTest {
-	@Test public void test1Coordinates() {
+	@Test public void test1NewNemoCoordinatesAreInOrigin() {
 		Nemo nemo = new Nemo();
 	    assertEquals( Arrays.asList(0,0), nemo.getCoordinates() );
 	  }
+	@Test public void test2NewNemoHasHeightZero() {
+		Nemo nemo = new Nemo();
+		assertEquals(0, nemo.getHeight());
+  }
+	@Test public void test3NewNemoHasDirectionZero() {
+		Nemo nemo = new Nemo();
+		assertEquals(0, nemo.getDirection());
+  }
+	
 }
