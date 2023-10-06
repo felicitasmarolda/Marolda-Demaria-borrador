2eb85bc82546f7e169353dabf4fb1ede86770003
Felicitas <fmarolda@udesa.edu.ar>
2023-10-06 16:08:40 -0300

    hhh

--- TP3/src/NemoTest.java
+++ TP3/src/NemoTest.java
@@ -1,6 +1,5 @@
 package nemo;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,106 +15,102 @@ import org.junit.jupiter.api.function.Executable;
 
 public class NemoTest {
 	@Test public void test00NewNemoCoordinatesAreInOrigin() {
-		// INICIALIZACION
 		Nemo nemo = new Nemo();
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(0, nemo.getYCoord());
-		assertEquals(0, nemo.getZCoord());
-		assertEquals(0, nemo.getDirection());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(0, nemo.yCoord);
+		assertEquals(0, nemo.zCoord);
+		assertEquals(0, nemo.direction);
 	  }
 	
 	@Test public void test01StaysTheSameWithEmptyCommand() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("");
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(0, nemo.getYCoord());
-		assertEquals(0, nemo.getZCoord());
-		assertEquals(0, nemo.getDirection());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(0, nemo.yCoord);
+		assertEquals(0, nemo.zCoord);
+		assertEquals(0, nemo.direction);
 	}
 
-	// MOVIMIENTOS BASICOS
 	@Test public void test02DescendsOneSpaceWithD() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("d");
-		assertEquals(-1, nemo.getZCoord());
+		assertEquals(-1, nemo.zCoord);
   }	
 
 	@Test public void test03CanNotGoAboveTheSurfaceWithUWhenInSurface() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("u");
-		assertEquals(0, nemo.getZCoord());
+		assertEquals(0, nemo.zCoord);
 	}
 
 	@Test public void test04CanChangeDirectionToLeft() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("l");
-		assertEquals(90, nemo.getDirection());
+		assertEquals(90, nemo.direction);
 	}
 	@Test public void test05CanChangeDirectionToRight() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("r");
-		assertEquals(270, nemo.getDirection());
+		assertEquals(270, nemo.direction);
 	}
 	@Test public void test06MovesForward() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("f");
-		assertEquals(1, nemo.getXCoord());
+		assertEquals(1, nemo.xCoord);
 	}
-	@Test public void testSuccesfulM() {
+	@Test public void test07SuccesfulM() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("m");
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(0, nemo.getYCoord());
-		assertEquals(0, nemo.getZCoord());
-		assertEquals(0, nemo.getDirection());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(0, nemo.yCoord);
+		assertEquals(0, nemo.zCoord);
+		assertEquals(0, nemo.direction);
 	}
 	
-	@Test public void testUnsuccesfulM() {
-		Nemo nemo = new Nemo();
-		nemo.commandsForNemo("ddm");
-		}
-
-	// FALTA M QUE NO ENTIENDO BIEN
-	
-	//MOVIMIENTOS MAS COMPLEJOS
-	@Test public void test07AscendsOneSpaceWithU() {
+	@Test public void test08AscendsOneSpaceWithU() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("ddu");
-		assertEquals(-1, nemo.getZCoord());
+		assertEquals(-1, nemo.zCoord);
   }	
-	@Test public void test08DirectionAndMoveForward() {
+	@Test public void test09DirectionAndMoveForward() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("rf");
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(-1, nemo.getYCoord());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(-1, nemo.yCoord);
 	}
-	@Test public void test09ThreeTypesOfCommands() {
+	@Test public void test10ThreeTypesOfCommands() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("lfd");
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(1, nemo.getYCoord());
-		assertEquals(-1, nemo.getZCoord());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(1, nemo.yCoord);
+		assertEquals(-1, nemo.zCoord);
 	}
-	@Test public void test10RepeatCommands() {
+	@Test public void test11RepeatCommands() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("ff");
-		assertEquals(2, nemo.getXCoord());
+		assertEquals(2, nemo.xCoord);
 	}	
-	@Test public void test11CanReturnToOriginWithOpositeCommands() {
+	@Test public void test12CanReturnToOriginWithOpositeCommands() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("rfllfdufdrruf");
-		assertEquals(0, nemo.getZCoord());
-		assertEquals(0, nemo.getXCoord());
-		assertEquals(0, nemo.getYCoord());
+		assertEquals(0, nemo.xCoord);
+		assertEquals(0, nemo.yCoord);
+		assertEquals(0, nemo.zCoord);
 	}
 	
-	@Test public void test12CanReachExpectedPoint() {
+	@Test public void test13CanReachExpectedPoint() {
 		Nemo nemo = new Nemo();
 		nemo.commandsForNemo("drmfddflfuff");
-		assertEquals(-2, nemo.getZCoord());
-		assertEquals(3, nemo.getXCoord());
-		assertEquals(-2, nemo.getYCoord());
+		assertEquals(-2, nemo.zCoord);
+		assertEquals(3, nemo.xCoord);
+		assertEquals(-2, nemo.yCoord);
 	}
+	@Test public void test14UnsuccesfulM() {
+		Nemo nemo = new Nemo();
+		assertEquals( "La capsula no puede ser liberada", 
+				assertThrows( Exception.class, () -> nemo.commandsForNemo("ddm"))
+				.getMessage());
+		}
 }
 
 
--- TP3/src/nemo.java
+++ TP3/src/nemo.java
@@ -5,93 +5,63 @@ import java.util.List;
 
 public class Nemo {
 	
-	private int zCoord;
-	private int xCoord;
-	private int yCoord;
-	private int direction;
+	public int zCoord;
+	public int xCoord;
+	public int yCoord;
+	public int direction;
 	
 	boolean capsule = false;
-
-//	public Nemo ( int height, int distX, int distY, int direction ) {
-//		this.ZCoord = height;
-//		this.xCoord = distX;
-//		this.xCoord = distX;
-//		this.direction = direction;
-//	}
 	
 	public Nemo () {
-	}
+	}        
 	
-    public int getXCoord() {
-        return xCoord;
-    }
-
-    public int getYCoord() {
-        return yCoord;
-    }
-    
-    public int getZCoord() {
-        return zCoord;
-    }
-    
-    public int getDirection() {
-        return direction;
-    }
-    
-	
-// subir mas de 0 te quedas en el cero
-    // falta variable capsula bolleana que diga si salio o no la cpasula, solo tirar error si estas a menos de -1
-    
 	// ver si comando perpetuado es string[] o string 
 	public void commandsForNemo(String string) {
 		for (int i = 0; i < string.length(); i++) {
             Character command = string.charAt(i);
             this.commandsForNemo(command);           
-	}}
-		public void commandsForNemo( Character command ) {
-			 if (command == 'd') {
-	            	this.zCoord = zCoord - 1;
-	            }
-	            if (command == 'u') {
-	            	if (this.zCoord <= -1) {
-	            		this.zCoord = zCoord + 1;
-	            	}
-	            	else {
-	            		this.zCoord = 0;
-	            	}
-	            }
-	            if (command == 'l') {
-	                	direction = (direction + 90 + 360) % 360;
-	             }
-	            if (command == 'r') {
-	            		direction = (direction - 90 + 360) % 360;
+	}
+		}
+	public void commandsForNemo( Character command ) {
+		 if (command == 'd') {
+            	this.zCoord = zCoord - 1;
+            }
+        if (command == 'u') {
+        	if (this.zCoord <= -1) {
+        		this.zCoord = zCoord + 1;
+        	}
+        	else {
+        		this.zCoord = 0;
+        	}
+        }
+        if (command == 'l') {
+            	direction = (direction + 90 + 360) % 360;
+         }
+        if (command == 'r') {
+        		direction = (direction - 90 + 360) % 360;
 
-	            	}
-	              
-	            if (command == 'f') {
-	            	if (direction == 0) {
-	            		this.xCoord = xCoord + 1;
-	            	} 
-	            	if (direction == 90) {
-	            		this.yCoord = yCoord + 1;
-	            	}
-	            	if (direction == 180) {
-	            		this.xCoord = xCoord - 1;
-	            	}
-	            	if (direction == 270) {
-	            		this.yCoord = yCoord - 1;
-	            	} 
-	        }
-	            if (command == 'm') {
-	            	if ( zCoord >= -1 && capsule == false) {
-	            		capsule = true;
-	            	}
-	            	else {
-	                    throw new UnsupportedOperationException( "La capsula no puede ser liberada" );
-	            	}
-	            	
-	    }
-	
-	// falta lo de sumergirse y salir o algo asi
-
+        	}
+          
+        if (command == 'f') {
+        	if (direction == 0) {
+        		this.xCoord = xCoord + 1;
+        	} 
+        	if (direction == 90) {
+        		this.yCoord = yCoord + 1;
+        	}
+        	if (direction == 180) {
+        		this.xCoord = xCoord - 1;
+        	}
+        	if (direction == 270) {
+        		this.yCoord = yCoord - 1;
+        	} 
+        }
+        if (command == 'm') {
+        	if ( zCoord >= -1 && capsule == false) {
+        		capsule = true;
+        	}
+        	else {
+                throw new UnsupportedOperationException( "La capsula no puede ser liberada" );
+        	}  	
+    }
 }}
