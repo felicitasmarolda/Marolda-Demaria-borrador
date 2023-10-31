dde4fae41d67939202fb4b0ccebc5f2a073998ee
Felicitas <fmarolda@udesa.edu.ar>
2023-10-31 14:16:48 -0300

    inicializar

--- TP4/linea/LineTest.java
+++ TP4/linea/LineTest.java
@@ -12,5 +12,17 @@ public class LineTest {
         assertEquals( expected, line.show() );
     }
 
-    @Test public void
+    @Test public void test01RedPlaysShownInBoard(){
+        Line line = new Line( 3, 3, 'C' );
+        line.playRedAt( 1 );
+        String expected = " C \n   \n   \n";
+        assertEquals( expected, line.show() );
+    }
+
+    @Test public void test02BluePlaysShownInBoard(){
+        Line line = new Line( 3, 3, 'C' );
+        line.playBlueAt( 1 );
+        String expected = " C \n   \n   \n";
+        assertEquals( expected, line.show() );
+    }
 }
