09d610e4dacc0c7083a2a6d3df7705eead5c3c41
Felicitas <fmarolda@udesa.edu.ar>
2023-11-03 16:14:48 -0300

    los typs estan vaios pero hay mas tests y cambio un poco Game

--- TP4/src/line/Triumph.java
+++ TP4/src/line/Triumph.java
@@ -1,5 +1,8 @@
 package juego.linea;
 
+import java.util.ArrayList;
+
 public abstract class Triumph {
+    public abstract boolean possibleTriumph(ArrayList<ArrayList<Character>> board, int column, int height);
 }
-//jj
+//
--- TP4/src/line/TypeA.java
+++ TP4/src/line/TypeA.java
@@ -1,6 +1,12 @@
 package juego.linea;
 
+import java.util.ArrayList;
+
 public class TypeA extends Triumph{
 
+    @Override
+    public boolean possibleTriumph(ArrayList<ArrayList<Character>> board, int column, int height) {
+        return false;
+    }
 }
 //triunfo en fila
